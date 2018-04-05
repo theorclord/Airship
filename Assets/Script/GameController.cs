c7b68aa8716e408dc3988e13635bdff9210e940f
theorclord <mikkeltheorclord@hotmail.com>
2018-04-05 20:55:12 +0200

    Added cloud spawning and left border

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+  public GameObject Cloud;
+  private float timeLastSpawn;
+	// Use this for initialization
+	void Start () {
+    timeLastSpawn = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time- timeLastSpawn > 1)
+    {
+      timeLastSpawn = Time.time;
+      Instantiate(Cloud, new Vector3(5f,Random.Range(-10f,10f)), Quaternion.identity);
+    }
+	}
+}
