16b94aace7cee01e749999d7304f0d360a351c5d
Theorclord <mikkeltheorclord@hotmail.com>
2018-04-05 15:42:13 +0200

    first commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+  private float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetKey(KeyCode.D))
+    {
+      Vector3 movement = new Vector3(1, 0);
+      transform.Translate(movement*speed);
+    }
+    if (Input.GetKey(KeyCode.A))
+    {
+      Vector3 movement = new Vector3(-1, 0);
+      transform.Translate(movement * speed);
+    }
+    if (Input.GetKey(KeyCode.W))
+    {
+      Vector3 movement = new Vector3(0, 1);
+      transform.Translate(movement * speed);
+    }
+    if (Input.GetKey(KeyCode.S))
+    {
+      Vector3 movement = new Vector3(0, -1);
+      transform.Translate(movement * speed);
+    }
+  }
+}
