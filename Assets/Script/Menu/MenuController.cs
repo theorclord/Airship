53f74e8be48bd2d417976e1f67be9d85913ca672
Mikkel <mikkeltheorclord@hotmail.com>
2019-05-16 13:54:01 +0200

    added sound

--- Assets/Script/Menu/MenuController.cs
+++ Assets/Script/Menu/MenuController.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
+
+  public GameObject MainMenu;
+  public GameObject OptionsMenu;
+  public Slider SoundSlider;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +17,7 @@ public class MenuController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+    GetComponent<AudioSource>().volume = PersistentData.Instance().SoundVal;
 	}
 
   public void StartGame()
@@ -27,6 +32,24 @@ public class MenuController : MonoBehaviour {
     // show credit panel
   }
 
+  public void Options()
+  {
+    MainMenu.SetActive(false);
+    OptionsMenu.SetActive(true);
+    SoundSlider.value = PersistentData.Instance().SoundVal;
+  }
+
+  public void MainMenuSet()
+  {
+    MainMenu.SetActive(true);
+    OptionsMenu.SetActive(false);
+  }
+
+  public void SetSoundLevel()
+  {
+    Debug.Log("The current sound value: " + SoundSlider.value);
+    PersistentData.Instance().SoundVal = SoundSlider.value;
+  }
   public void ExitGame()
   {
     Application.Quit();
